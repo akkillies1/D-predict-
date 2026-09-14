bd7434d9e8d288906f0dfd17c531586fdd9ef9d1
akkillies1 <dcodeakhil@gmail.com>
2026-09-14 22:36:23 +0530

    feat: make live terminal the default dashboard

--- dashboard/client/src/App.tsx
+++ dashboard/client/src/App.tsx
@@ -2,19 +2,10 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home";
+import Terminal from "./pages/Terminal";
 
 function App() {
-  return (
-    <ErrorBoundary>
-      <ThemeProvider defaultTheme="dark">
-        <TooltipProvider>
-          <Toaster />
-          <Home />
-        </TooltipProvider>
-      </ThemeProvider>
-    </ErrorBoundary>
-  );
+  return <ErrorBoundary><ThemeProvider defaultTheme="dark"><TooltipProvider><Toaster/><Terminal/></TooltipProvider></ThemeProvider></ErrorBoundary>;
 }
 
 export default App;
