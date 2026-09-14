7c160fe2f345fb050a0789dfa89e0a1b9b851943
akkillies1 <dcodeakhil@gmail.com>
2026-09-14 22:47:01 +0530

    feat: surface research intelligence in dashboard

--- dashboard/client/src/App.tsx
+++ dashboard/client/src/App.tsx
@@ -1,11 +1,12 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ErrorBoundary from "./components/ErrorBoundary";
+import ResearchPanel from "./components/ResearchPanel";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Terminal from "./pages/Terminal";
 
 function App() {
-  return <ErrorBoundary><ThemeProvider defaultTheme="dark"><TooltipProvider><Toaster/><Terminal/></TooltipProvider></ThemeProvider></ErrorBoundary>;
+  return <ErrorBoundary><ThemeProvider defaultTheme="dark"><TooltipProvider><Toaster/><Terminal/><ResearchPanel/></TooltipProvider></ThemeProvider></ErrorBoundary>;
 }
 
 export default App;
