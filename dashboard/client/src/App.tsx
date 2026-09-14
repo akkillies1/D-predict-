1b87fbd764fdec74fbbcb85e5e751cd61b869430
akkillies1 <dcodeakhil@gmail.com>
2026-09-15 01:43:21 +0530

    add IPO analyzer to dashboard

--- dashboard/client/src/App.tsx
+++ dashboard/client/src/App.tsx
@@ -1,12 +1,13 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ErrorBoundary from "./components/ErrorBoundary";
+import IPOAnalyzer from "./components/IPOAnalyzer";
 import ResearchPanel from "./components/ResearchPanel";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Terminal from "./pages/Terminal";
 
 function App() {
-  return <ErrorBoundary><ThemeProvider defaultTheme="dark"><TooltipProvider><Toaster/><Terminal/><ResearchPanel/></TooltipProvider></ThemeProvider></ErrorBoundary>;
+  return <ErrorBoundary><ThemeProvider defaultTheme="dark"><TooltipProvider><Toaster/><Terminal/><ResearchPanel/><IPOAnalyzer/></TooltipProvider></ThemeProvider></ErrorBoundary>;
 }
 
 export default App;
