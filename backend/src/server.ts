d7ee263c56c7cb7e8526e942da1751129cafd21e
akkillies1 <dcodeakhil@gmail.com>
2026-09-15 11:53:27 +0530

    fix: correct trade thesis import path

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import pg from "pg";
-import { buildCausalTradeThesis } from "./services/tradeThesis.js";
+import { buildCausalTradeThesis } from "../src/tradeThesis.js";
 
 dotenv.config();
 
