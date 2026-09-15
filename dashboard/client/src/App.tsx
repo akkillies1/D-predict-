f8602404eb3a77c65896335a8e298d3d04f1aecc
akkillies1 <dcodeakhil@gmail.com>
2026-09-15 09:30:55 +0530

    make decision dashboard the primary UI

--- dashboard/client/src/App.tsx
+++ dashboard/client/src/App.tsx
@@ -1,13 +1,13 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ErrorBoundary from "./components/ErrorBoundary";
+import DecisionDashboard from "./components/DecisionDashboard";
 import IPOAnalyzer from "./components/IPOAnalyzer";
 import ResearchPanel from "./components/ResearchPanel";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import Terminal from "./pages/Terminal";
 
 function App() {
-  return <ErrorBoundary><ThemeProvider defaultTheme="dark"><TooltipProvider><Toaster/><Terminal/><ResearchPanel/><IPOAnalyzer/></TooltipProvider></ThemeProvider></ErrorBoundary>;
+  return <ErrorBoundary><ThemeProvider defaultTheme="dark"><TooltipProvider><Toaster/><DecisionDashboard/><ResearchPanel/><IPOAnalyzer/></TooltipProvider></ThemeProvider></ErrorBoundary>;
 }
 
 export default App;
