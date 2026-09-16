fe66f11be578cde8907a75837f8d4b9e74ebaaf4
akkillies1 <dcodeakhil@gmail.com>
2026-09-16 13:22:53 +0530

    Add database settings to dashboard

--- dashboard/client/src/App.tsx
+++ dashboard/client/src/App.tsx
@@ -1,13 +1,14 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ErrorBoundary from "./components/ErrorBoundary";
+import DatabaseSettings from "./components/DatabaseSettings";
 import DecisionDashboard from "./components/DecisionDashboard";
 import IPOAnalyzer from "./components/IPOAnalyzer";
 import ResearchPanel from "./components/ResearchPanel";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
 function App() {
-  return <ErrorBoundary><ThemeProvider defaultTheme="dark"><TooltipProvider><Toaster/><DecisionDashboard/><ResearchPanel/><IPOAnalyzer/></TooltipProvider></ThemeProvider></ErrorBoundary>;
+  return <ErrorBoundary><ThemeProvider defaultTheme="dark"><TooltipProvider><Toaster/><DecisionDashboard/><ResearchPanel/><IPOAnalyzer/><DatabaseSettings/></TooltipProvider></ThemeProvider></ErrorBoundary>;
 }
 
 export default App;
