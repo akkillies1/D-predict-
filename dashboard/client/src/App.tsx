c2646f25aeb2f2352fff589debaa1497255fd779
akkillies1 <dcodeakhil@gmail.com>
2026-09-16 14:11:15 +0530

    Mount live option chain paper trading

--- dashboard/client/src/App.tsx
+++ dashboard/client/src/App.tsx
@@ -4,12 +4,13 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import DatabaseSettings from "./components/DatabaseSettings";
 import DecisionDashboard from "./components/DecisionDashboard";
 import IPOAnalyzer from "./components/IPOAnalyzer";
+import OptionChainTradingPanel from "./components/OptionChainTradingPanel";
 import ResearchPanel from "./components/ResearchPanel";
 import ShadowTradingPanel from "./components/ShadowTradingPanel";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
 function App() {
-  return <ErrorBoundary><ThemeProvider defaultTheme="dark"><TooltipProvider><Toaster/><DecisionDashboard/><ShadowTradingPanel/><ResearchPanel/><IPOAnalyzer/><DatabaseSettings/></TooltipProvider></ThemeProvider></ErrorBoundary>;
+  return <ErrorBoundary><ThemeProvider defaultTheme="dark"><TooltipProvider><Toaster/><DecisionDashboard/><OptionChainTradingPanel/><ShadowTradingPanel/><ResearchPanel/><IPOAnalyzer/><DatabaseSettings/></TooltipProvider></ThemeProvider></ErrorBoundary>;
 }
 
 export default App;
